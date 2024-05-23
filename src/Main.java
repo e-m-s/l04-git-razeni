20d21c24149d0bd2d6c3516eb9dcf2dbff635920
Martin Simunek <public@e-mis.cz>
2024-05-23 22:38:55 +0200

    add demo of sorting list of integers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(10);
+        integerList.add(5);
+        integerList.add(7);
+
+        System.out.println("Před seřazením: " + integerList);
+        integerList.sort(null);
+        // Alternativní zápis - funguje i na jiné typy kolekcí:
+        // Collections.sort(integerList);
+        System.out.println("Po seřazení: " + integerList);
     }
 }
